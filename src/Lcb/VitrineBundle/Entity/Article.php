e32d086f9a4d948865b2fdbe23f65df74d696d43
Baulieu <b.de.reboul@gmail.com>
2015-07-17 19:37:40 +0200

    création de l'entité Article

---
+++ src/Lcb/VitrineBundle/Entity/Article.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Lcb\VitrineBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Lcb\VitrineBundle\Entity\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="parties", type="array")
+     */
+    private $parties;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="images", type="array")
+     */
+    private $images;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     * @return Article
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string 
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     * @return Article
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Article
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set parties
+     *
+     * @param array $parties
+     * @return Article
+     */
+    public function setParties($parties)
+    {
+        $this->parties = $parties;
+
+        return $this;
+    }
+
+    /**
+     * Get parties
+     *
+     * @return array 
+     */
+    public function getParties()
+    {
+        return $this->parties;
+    }
+
+    /**
+     * Set images
+     *
+     * @param array $images
+     * @return Article
+     */
+    public function setImages($images)
+    {
+        $this->images = $images;
+
+        return $this;
+    }
+
+    /**
+     * Get images
+     *
+     * @return array 
+     */
+    public function getImages()
+    {
+        return $this->images;
+    }
+}
